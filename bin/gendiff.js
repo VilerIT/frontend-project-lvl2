99f93a9fbb07ddce9d6aaba97b1dba8f4b957087
VilerIT <vilerinftech@gmail.com>
2021-02-03 17:04:12 +0400

    create gendiff.js

---
+++ bin/gendiff.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program.description('Compares two configuration files and how a difference.');
+
+program.version('1.0.0');
+
+program.parse();
