e866fe74efc43c6f26a39b3508a2268b15c6eb0c
VilerIT <vilerinftech@gmail.com>
2021-02-03 17:22:30 +0400

    correct description

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,7 @@ import { program } from 'commander';
 
 program
   .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and how a difference.')
+  .description('Compares two configuration files and shows a difference.')
   .version('1.0.0')
   .option('-f, --format [type]', 'output format')
   .parse();
