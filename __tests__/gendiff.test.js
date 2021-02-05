031df19f7683be3ce19169926f96ba928c763f1d
VilerIT <vilerinftech@gmail.com>
2021-02-05 19:32:59 +0400

    update tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,24 +1,21 @@
-import path from 'path';
 import genDiff from '../src/index.js';
 
+const getDiff = (format) => genDiff(
+  `__tests__/examples/before.${format}`,
+  `__tests__/examples/after.${format}`,
+);
+
 describe('genDiff', () => {
-  const expected = '{\n'
-  + '\t- edited: 1\n'
-  + '\t+ edited: 11\n'
-  + '\t- deleted: 0\n'
-  + '\tsame: 2\n'
-  + '\t+ new: 5\n'
-  + '}';
+  test('JSON', () => {
+    const expected = '{\n'
+      + '\t- edited: 1\n'
+      + '\t+ edited: 11\n'
+      + '\t- deleted: 0\n'
+      + '\tsame: 2\n'
+      + '\t+ new: 5\n'
+      + '}';
 
-  test('JSON, relative path', () => {
-    const actual = genDiff('__tests__/examples/before.json', '__tests__/examples/after.json');
-    expect(actual).toEqual(expected);
-  });
-
-  test('JSON, absolute path', () => {
-    const absolutePath = path.resolve(process.cwd(), '__tests__/examples');
-    const actual = genDiff(`${absolutePath}/before.json`, `${absolutePath}/after.json`);
-
+    const actual = getDiff('json');
     expect(actual).toEqual(expected);
   });
 });
