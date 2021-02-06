a5f78a88282c1250f9f09ad428a20cc2ede0c9d9
VilerIT <vilerinftech@gmail.com>
2021-02-06 20:13:09 +0400

    move expected to fixtures

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,29 +1,18 @@
 import { fileURLToPath } from 'url';
 import path, { dirname } from 'path';
+import fs from 'fs';
 import genDiff from '../src/gendiff.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getDiff = (format) => {
-  const pathToFixtures = path.join(__dirname, '..', '__fixtures__');
-  return genDiff(
-    path.join(pathToFixtures, `before.${format}`),
-    path.join(pathToFixtures, `after.${format}`),
-  );
-};
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe('genDiff', () => {
   test('JSON', () => {
-    const expected = '{\n'
-      + '  - edited: 1\n'
-      + '  + edited: 11\n'
-      + '  - deleted: 0\n'
-      + '  same: 2\n'
-      + '  + new: 5\n'
-      + '}';
+    const expected = fs.readFileSync(getFixturePath('expected-json.txt')).toString();
 
-    const actual = getDiff('json');
+    const actual = genDiff(getFixturePath('before.json'), getFixturePath('after.json'));
     expect(actual).toEqual(expected);
   });
 });
