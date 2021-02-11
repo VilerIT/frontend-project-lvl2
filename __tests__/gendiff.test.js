89e920b74c0708797114af657dc8ad20ef250f02
VilerIT <vilerinftech@gmail.com>
2021-02-11 21:26:22 +0400

    add tests for json format

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 describe.each(['json', 'yml'])('%s', (extension) => {
-  test.each(['stylish', 'plain'])('%s', (format) => {
+  test.each(['stylish', 'plain', 'json'])('%s', (format) => {
     const expected = readFile(getFixturePath(`expected-${format}.txt`));
     const actual = genDiff(getFixturePath(`before.${extension}`), getFixturePath(`after.${extension}`), format);
     expect(actual).toBe(expected);
