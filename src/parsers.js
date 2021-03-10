74e1923b834d20329f2581f86511367a66355682
VilerIT <vilerinftech@gmail.com>
2021-03-10 23:23:27 +0400

    add ini support

--- src/parsers.js
+++ src/parsers.js
@@ -1,16 +1,23 @@
 import yaml from 'js-yaml';
+import ini from 'ini';
 
 const parseJson = (content) => JSON.parse(content);
 
 const parseYaml = (content) => yaml.load(content);
+
+const parseIni = (content) => ini.parse(content);
 
 const parse = (content, extname) => {
   if (extname === '.json') {
     return parseJson(content);
   }
 
-  if (extname === '.yml') {
+  if (extname === '.yml' || extname === '.yaml') {
     return parseYaml(content);
+  }
+
+  if (extname === '.ini') {
+    return parseIni(content);
   }
 
   return null;
