a1013c2de60c2b24087bdf3824537336234eada0
VilerIT <vilerinftech@gmail.com>
2021-04-23 21:12:15 +0400

    lint code

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ import format from './src/formatters/index.js';
 
 /**
  * The function to calculate differences between two configuration files (json, yaml, and ini).
- * You can calculate differences between files with different extensions, e.g. ``before.ini`` and ``after.yml``.
- * 
+ * You can calculate differences between files with different extensions,
+ * e.g. ``before.ini`` and ``after.yml``.
+ *
  * @param {String} path1 The path to the first file. Can be relative or absolute.
  * @param {String} path2 The path to the second file. Can be relative or absolute.
  * @param {String} formatType
  *
  * Optional. There are three available format types:
  *
- * - **stylish** - pretty-printed JSON-like format with indents and braces for nested objects. Used by default.
+ * - **stylish** - pretty-printed JSON-like format with indents
+ * and braces for nested objects. Used by default.
  * - **plain** - simplistic format, which represents the list of changed properties.
  * - **json** - the result of ``JSON.stringify`` of AST.
  * @returns {String} The calculated diff.
